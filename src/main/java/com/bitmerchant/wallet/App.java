44b727a146b3eb0a408bf3848283bb380af10ec7
tyler <happydooby@gmail.com>
2014-12-16 17:14:37 -0500

    first commit

---
+++ src/main/java/com/bitmerchant/wallet/App.java
@@ -0,0 +1,13 @@
+package com.bitmerchant.wallet;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
